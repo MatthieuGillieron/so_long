f7409122ffa31a2392ef891f8ed4614642f4cc58
MatthieuGillieron <gillieron.matthieu@ik.me>
2024-12-02 09:44:56 +0100

    clean

--- src/move.c
+++ src/move.c
@@ -6,8 +6,77 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 10:29:43 by mg                #+#    #+#             */
-/*   Updated: 2024/11/29 10:58:44 by mg               ###   ########.fr       */
+/*   Updated: 2024/12/02 09:44:18 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
+
+void	player_position(t_game *game)
+{
+	size_t	y;
+	size_t	x;
+
+	y = 0;
+	while (game->map[y])
+	{
+		x = 0;
+		while (game->map[y][x])
+		{
+			if(game->map[y][x] == 'P')
+			{
+				game->player_x = x;
+				game->player_y = y;
+				return;
+			}
+			x++;
+		}
+		y++;
+	}
+}
+
+void	move_player(t_game *game, int dx, int dy)
+{
+	int	new_x;
+	int	new_y;
+
+	new_x = game->player_x + dx;
+	new_y =game->player_y +dy;
+
+	if (game->map[new_y][new_x] != '1')
+	{
+		game->map[game->player_y][game->player_x] = '0';
+		game->map[new_y][new_x] = 'P';
+
+		game->player_x = new_x;
+		game->player_y = new_y;
+
+		mlx_clear_window(game->mlx_ptr, game->win_ptr);
+		draw_map(game->map, game->mlx_ptr, game->win_ptr, game->textures);
+	}
+}
+
+int	keyboard(int input, t_game *game)
+{
+	if (input == 13 || input == 0 || input == 1 || input == 2)
+	{
+		game->input_count++;
+		ft_printf("Input :  %d\n", game->input_count);
+	}
+	if (input == 13)
+		move_player(game, 0, -1);
+	else if (input == 0)
+		move_player(game, -1, 0);
+	else if (input == 1)
+		move_player(game, 0, 1);
+	else if (input == 2)
+		move_player(game, 1, 0);
+	else if (input == 53)
+	{
+		mlx_destroy_window(game->mlx_ptr, game->win_ptr);
+		free_map(game->map);
+		free(game->textures);
+		exit (0); // verif si exit ok pour 42
+	}
+	return (0);
+}
